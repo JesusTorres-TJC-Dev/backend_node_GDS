d9c507308a0fba7d7364777f693c2b2d5b8a98ff
JesusTorres-TJC-Dev <TJC.dev.1997@gmail.com>
2022-11-16 19:23:30 -0400

    add nginx service

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,8 @@ import db from "./models"
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors({
-    origin: ['*']
-}))
+app.enable("trust proxy")
+app.use(cors({}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
